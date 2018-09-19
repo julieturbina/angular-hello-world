7e3e6a1659ecf93e077e44f470ffecab034dffad
Juliet Urbina <julieturbina8@gmail.com>
2018-09-19 19:44:32 -0400

    first app using imtetpolation to display a class property working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export default class AppComponent {
-  title = 'angular-helloworld';
+  title = 'Welcoms to Angular-helloworld';
   content = 'A new world is waiting for you!';
   animals: Array<Object> = [
     {
